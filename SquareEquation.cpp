af029e390e90839304241c10fea5bb61e8b0b334
LevShepelev <70482448+LevShepelev@users.noreply.github.com>
2020-09-06 22:50:52 +0300

    Update SquareEquation.cpp

--- SquareEquation.cpp
+++ SquareEquation.cpp
@@ -1,55 +1,58 @@
 #include <stdio.h>
 #include <math.h>
 
-
+const double epsilon = 1e-7;
 int Solvelinear (double a, double b, double* adr1)
     {
-    printf("Your equation if linear");
-    if (a != 0)
+    printf("Your equation if linear, ");
+    if (fabs (a) > epsilon)
         {
         printf ("1 root, x = %lg\n", -b / a);
         *adr1 = -b / a;
         }
-        if ((a == 0) && (b == 0))
-    printf ("Infinite number of roots");
-    if ((a == 0) && (b != 0))
-    printf (" No roots");
+    if ((fabs (a) < epsilon) && (fabs (b) < epsilon))
+        printf ("Infinite number of roots");
+    if ((fabs (a) < epsilon) && (fabs (b) > epsilon))
+        printf ("No roots");
     }
 int SolveSquare (double a, double b, double c, double* adr1, double* adr2)
     {
     double discriminant = b*b - 4*a*c;
-    if (a != 0)
+    if (fabs (a) > epsilon)
         {
-        if (discriminant > 0)
+        if (discriminant > epsilon)
             {
             *adr1 = (-b - sqrt (discriminant)) / (2*a);
             *adr2 = (-b + sqrt (discriminant)) / (2*a);
             return 2;
             }
-        if (discriminant == 0)
+        if (fabs (discriminant) < epsilon)
             {
             *adr1 = b / (2*a);
             return 1;
             }
-        if (discriminant < 0)
+        if (discriminant < -epsilon)
             {
-            printf ("No roots ");
+            printf ("No roots");
             return 0;
             }
         }
         else
-        Solvelinear (b,c, adr1);
+        Solvelinear (b, c, adr1);
     }
 
 int main()
     {
-    printf ("Square Equation Solver 2020\n", "write  coefficient a, b, ñ");
-    double a = 0;
-    double b = 0;
-    double c = 0;
-    double x1 = 0;
-    double x2 = 0;
-    scanf ("%lg%lg%lg", &a, &b, &c);
+    printf ("Square Equation Solver 2020\nWrite coefficient a, b, c\n");
+    double a = 0, b = 0, c = 0;
+    double x1 = 0, x2 = 0;
+    int sc = scanf ("%lg%lg%lg", &a, &b, &c);
+    printf ("sc = %d\n", sc);
+    while (sc != 3)
+        {
+        sc = scanf ("%lg%lg%lg", &a, &b, &c);
+        printf ("Incorrect input, try again\n");
+        }
     int nRoots = SolveSquare (a, b, c, &x1, &x2);
     if (nRoots == 2)
         {
@@ -60,6 +63,7 @@ int main()
         printf ("x = %lg\n", x1) ;
         }
     }
+
 
 
 
